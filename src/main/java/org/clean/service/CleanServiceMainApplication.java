406118cf88261d2192f318b788a339413656e72d
khoavale-uno <khoavale.kms@uno.bank>
2025-09-20 22:15:03 +0700

    Init project

---
+++ src/main/java/org/clean/service/CleanServiceMainApplication.java
@@ -0,0 +1,11 @@
+package org.clean.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanServiceMainApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(CleanServiceMainApplication.class, args);
+  }
+}
